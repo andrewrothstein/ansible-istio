4f9a079d04d939b16d46673ad12828f9e3dbd013
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-06-24 08:16:28 -0500

    up to v1.2.0

---
+++ dl-checksum.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+VER=1.2.0
+MIRROR=https://github.com/istio/istio/releases/download/$VER
+
+dl()
+{
+    OS=$1
+    ARCHIVE=$2
+    URL=$MIRROR/istio-$VER-$OS.$ARCHIVE.sha256
+    printf "  # %s\n" $URL
+    printf "  %s: sha256:%s\n" $OS `curl -sSL $URL | awk '{print $1}'`
+}
+
+printf "'%s':\n" $VER
+dl linux tar.gz
+dl osx tar.gz
+dl win zip
+
